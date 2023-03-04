b6079021a5532f938cfb6b6eeb59083ba63d0451
yangmaoting <524250484@qq.com>
2023-03-04 22:10:47 +0800

    提交智能重组

--- app/src/main/java/com/sundayting/composeinsimplelanguage/one/OneActivity.kt
+++ app/src/main/java/com/sundayting/composeinsimplelanguage/one/OneActivity.kt
@@ -1,9 +1,108 @@
 package com.sundayting.composeinsimplelanguage.one
 
+import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.sundayting.composeinsimplelanguage.ui.ChapterBackground
+import com.sundayting.composeinsimplelanguage.ui.commonChapterModifier
 
-class OneActivity:ComponentActivity() {
+class OneActivity : ComponentActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ChapterOneScreen(onClickNext = {
 
+            })
+        }
+    }
 
+}
+
+@Composable
+fun ChapterOneScreen(onClickNext: () -> Unit = {}) {
+    var blockOneNum by remember { mutableStateOf(0) }
+    var blockTwoNum by remember { mutableStateOf(0) }
+    val state = rememberScrollState()
+    ChapterBackground(
+        modifier = commonChapterModifier,
+        title = "智能重组",
+        desc = "本案例可以让你清楚得体验到智能重组的高效性",
+        onClickNext = onClickNext
+    ) {
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(state),
+        ) {
+
+            ComposeBlock(
+                num = blockOneNum,
+                logTag = "方块1",
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(1f, false)
+                    .background(color = Color.Gray.copy(alpha = 0.2f)),
+                onClick = {
+                    blockOneNum++
+                }
+            )
+            ComposeBlock(
+                num = blockTwoNum,
+                logTag = "方块2",
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(1f, false)
+                    .background(color = Color.Green.copy(alpha = 0.2f)),
+                onClick = {
+                    blockTwoNum++
+                }
+            )
+        }
+    }
+
+}
+
+/**
+ * 体验智能重组的可重组函数
+ * @param num 显示的数字
+ * @param logTag 日志标签，用于查看Log.d
+ */
+@Composable
+private fun ComposeBlock(
+    num: Int,
+    logTag: String,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = {},
+) {
+
+    Box(
+        modifier = modifier.clickable { onClick() },
+        contentAlignment = Alignment.Center
+    ) {
+        //此代码的作用在第一章中不需要理解，你只需要知道，当本可重组函数进入重组的时候，SideEffect内部的lambda函数会执行就可以了
+        SideEffect {
+            Log.d("临时测试", "${logTag}发生了重组")
+        }
+        Text("$num")
+    }
 }
--- app/src/main/java/com/sundayting/composeinsimplelanguage/ui/Common.kt
+++ app/src/main/java/com/sundayting/composeinsimplelanguage/ui/Common.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
@@ -21,15 +22,18 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+val commonChapterModifier = Modifier
+    .fillMaxSize()
+    .background(color = Color.White)
+    .padding(10.dp)
+
 @Composable
 @Preview
 fun PreviewChapterBackground() {
     ChapterBackground(
         title = "第一章",
         desc = "我们随便说说",
-        modifier = Modifier
-            .fillMaxSize()
-            .background(color = Color.White)
+        modifier = commonChapterModifier
     ) {
         Box(
             modifier = Modifier
@@ -47,28 +51,33 @@ fun ChapterBackground(
     title: String,
     desc: String,
     modifier: Modifier = Modifier,
+    onClickNext: () -> Unit = {},
     content: @Composable ColumnScope.() -> Unit,
 ) {
 
-    Column(
-        modifier = modifier
-            .padding(10.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(text = title, fontSize = 30.sp, fontWeight = FontWeight.Bold)
-        Text(text = desc, fontSize = 20.sp)
-        Spacer(Modifier.height(20.dp))
-        Card(
-            modifier = Modifier
-                .weight(1f, false)
-                .fillMaxSize(),
-            colors = CardDefaults.cardColors(
-                containerColor = Color.Transparent
-            ),
-            border = BorderStroke(1.dp, color = Color.Gray)
+    Box(modifier = modifier) {
+        Button(onClick = onClickNext, modifier = Modifier.align(Alignment.TopEnd)) {
+            Text("下一章", fontSize = 15.sp)
+        }
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            content()
+            Text(text = title, fontSize = 30.sp, fontWeight = FontWeight.Bold)
+            Text(text = desc, fontSize = 20.sp)
+            Spacer(Modifier.height(20.dp))
+            Card(
+                modifier = Modifier
+                    .weight(1f, false)
+                    .fillMaxSize(),
+                colors = CardDefaults.cardColors(
+                    containerColor = Color.Transparent
+                ),
+                border = BorderStroke(1.dp, color = Color.Gray)
+            ) {
+                content()
+            }
         }
     }
+
 
 }
