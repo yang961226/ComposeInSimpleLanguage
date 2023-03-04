3a01c618f845b2ff99d5b74ba850063e0d4495bf
yangmaoting <524250484@qq.com>
2023-03-04 21:05:57 +0800

    提交章节背景

---
+++ app/src/main/java/com/sundayting/composeinsimplelanguage/ui/Common.kt
@@ -0,0 +1,74 @@
+package com.sundayting.composeinsimplelanguage.ui
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+@Preview
+fun PreviewChapterBackground() {
+    ChapterBackground(
+        title = "第一章",
+        desc = "我们随便说说",
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+
+        )
+    }
+}
+
+/**
+ * 每一章的背景
+ */
+@Composable
+fun ChapterBackground(
+    title: String,
+    desc: String,
+    modifier: Modifier = Modifier,
+    content: @Composable ColumnScope.() -> Unit,
+) {
+
+    Column(
+        modifier = modifier
+            .padding(10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = title, fontSize = 30.sp, fontWeight = FontWeight.Bold)
+        Text(text = desc, fontSize = 20.sp)
+        Spacer(Modifier.height(20.dp))
+        Card(
+            modifier = Modifier
+                .weight(1f, false)
+                .fillMaxSize(),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.Transparent
+            ),
+            border = BorderStroke(1.dp, color = Color.Gray)
+        ) {
+            content()
+        }
+    }
+
+}
